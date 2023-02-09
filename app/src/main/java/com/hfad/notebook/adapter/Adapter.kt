7a52e6ec0854501d9291398dacca5b1d773e9ad4
dmytrobilash <dmytro.bilash@nure.ua>
2023-02-10 00:48:44 +0200

    adapter

--- app/src/main/java/com/hfad/notebook/adapter/Adapter.kt
+++ app/src/main/java/com/hfad/notebook/adapter/Adapter.kt
@@ -1,4 +1,40 @@
 package com.hfad.notebook.adapter
 
-class Adapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hfad.notebook.R
+import com.hfad.notebook.model.Note
+import kotlinx.android.synthetic.main.deleting_fragment.view.*
+import kotlinx.android.synthetic.main.note_items.view.*
+import kotlinx.android.synthetic.main.note_items.view.title
+
+class Adapter : RecyclerView.Adapter<Adapter.NoteViewHolder>() {
+
+    var listNote = emptyList<Note>()
+    class NoteViewHolder(view:View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.note_items, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.itemView.title.text = listNote[position].title
+        //holder.itemView.description.text = listNote[position].description
+        //holder.itemView.creation_time.text = listNote[position].creationTime
+    }
+    override fun getItemCount(): Int {
+        return listNote.size;
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setList(list: List<Note>){
+        listNote = list
+        notifyDataSetChanged()
+    }
 }
