eaf4b5a625099199b6a941dd01813061bd4b971e
dmytrobilash <dmytro.bilash@nure.ua>
2023-02-11 22:46:25 +0200

    interfaces

--- app/src/main/java/com/hfad/notebook/db/Dao.kt
+++ app/src/main/java/com/hfad/notebook/db/Dao.kt
@@ -1,4 +1,24 @@
 package com.hfad.notebook.db
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import androidx.room.Dao
+import com.hfad.notebook.model.Note
+
+@Dao
 interface Dao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM note_table")
+    fun getAll():LiveData<List<Note>>
+
+
+
+
+
 }
--- app/src/main/java/com/hfad/notebook/db/Repository.kt
+++ app/src/main/java/com/hfad/notebook/db/Repository.kt
@@ -1,4 +1,10 @@
 package com.hfad.notebook.db
 
+import androidx.lifecycle.LiveData
+import com.hfad.notebook.model.Note
+
 interface Repository {
+    val allNotes:LiveData<List<Note>>
+    suspend fun insert(note:Note, onSuccess:() -> Unit)
+    suspend fun delete(note:Note, onSuccess:() -> Unit)
 }
