cb0e81c7852bb26a921544714a48e8464a10f433
dmytrobilash <dmytro.bilash@nure.ua>
2023-02-18 20:35:06 +0200

    priority add

--- app/src/main/java/com/hfad/notebook/views/AddFragment.kt
+++ app/src/main/java/com/hfad/notebook/views/AddFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -39,6 +40,15 @@ class AddFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.O)
     private fun init() {
         val viewModel = ViewModelProvider(this).get(AddViewModel::class.java)
+
+        val priorities: Array<String> = arrayOf("0", "1", "2", "3")
+        val adapter = ArrayAdapter(
+            APP,
+            R.layout.note_items,
+            priorities
+        )
+        binding.filledExposed.setAdapter(adapter)
+
         binding.btnAdd.setOnClickListener {
             val title = binding.editTitle.editText?.text.toString()
             val description = binding.editDescription.editText?.text.toString()
@@ -46,7 +56,9 @@ class AddFragment : Fragment() {
                 Note(
                     title = title,
                     description = description,
-                    creationTime = LocalTime.now().toString().substring(0, 8)+  " " + LocalDate.now().toString().substring(5)
+                    creationTime = LocalTime.now().toString().substring(0, 8) + " " + LocalDate.now().toString().substring(5),
+                    taskPriority = binding.filledExposed.text.toString().toInt(),
+                    //todo finished time()
                 )
             ) {}
             APP.navController.navigate(R.id.action_addFragment_to_startFragment)
