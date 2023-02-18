1d380f9406313b4553c1f65f913ca4913b6cbb1b
dmytrobilash <dmytro.bilash@nure.ua>
2023-02-18 22:52:35 +0200

    date and time picker for end date of note

--- app/src/main/java/com/hfad/notebook/views/AddFragment.kt
+++ app/src/main/java/com/hfad/notebook/views/AddFragment.kt
@@ -1,12 +1,15 @@
 package com.hfad.notebook.views
 
 import APP
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.DatePicker
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -17,6 +20,7 @@ import com.hfad.notebook.model.Note
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.util.*
 
 class AddFragment : Fragment() {
 
@@ -39,15 +43,56 @@ class AddFragment : Fragment() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun init() {
+        var mYear: Int;
+        var mMonth: Int;
+        var mDay: Int;
+        var mHour: Int;
+        var mMinute: Int
         val viewModel = ViewModelProvider(this).get(AddViewModel::class.java)
 
         val priorities: Array<String> = arrayOf("0", "1", "2", "3")
+
         val adapter = ArrayAdapter(
             APP,
-            R.layout.note_items,
+            R.layout.drop_down_items,
             priorities
         )
         binding.filledExposed.setAdapter(adapter)
+
+        binding.timePickerBtn.setOnClickListener {
+            val calendar: Calendar = Calendar.getInstance()
+            mHour = calendar.get(Calendar.HOUR_OF_DAY)
+            mMinute = calendar.get(Calendar.MINUTE)
+            val tpd = TimePickerDialog(
+                APP,
+                TimePickerDialog.OnTimeSetListener() { view, hourOfDay, minute ->
+                    binding.timePickerInput.editText?.setText("$hourOfDay:$minute")
+                },
+                mHour,
+                mMinute,
+                false
+            )
+            tpd.show()
+        }
+
+        binding.datePickerBtn.setOnClickListener {
+            val calendar: Calendar = Calendar.getInstance()
+            mYear = calendar.get(Calendar.YEAR)
+            mMonth = calendar.get(Calendar.MONTH)
+            mDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val dpd = DatePickerDialog(
+                APP,
+                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                    // Update the text of the appropriate view with the selected date
+                    binding.datePickerInput.editText?.setText("$year/${month + 1}/$dayOfMonth")
+                },
+                mYear,
+                mMonth,
+                mDay
+            )
+            dpd.show()
+        }
 
         binding.btnAdd.setOnClickListener {
             val title = binding.editTitle.editText?.text.toString()
@@ -56,7 +101,8 @@ class AddFragment : Fragment() {
                 Note(
                     title = title,
                     description = description,
-                    creationTime = LocalTime.now().toString().substring(0, 8) + " " + LocalDate.now().toString().substring(5),
+                    creationTime = LocalTime.now().toString()
+                        .substring(0, 8) + " " + LocalDate.now().toString().substring(5),
                     taskPriority = binding.filledExposed.text.toString().toInt(),
                     //todo finished time()
                 )
